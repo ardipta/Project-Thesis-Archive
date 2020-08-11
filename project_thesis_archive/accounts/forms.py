1c5cf1332de87d7e78d1a056f0de8ac444767e61
ashikbhuyan <ashikbhuyan.swe.diu@gmail.com>
2020-08-11 16:41:05 +0600

    added login for student and teacher => accounts

--- project_thesis_archive/accounts/forms.py
+++ project_thesis_archive/accounts/forms.py
@@ -1,22 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 from accounts.models import Account
 
-
-# class TeacherRegistration(UserCreationForm):
-#     email = forms.EmailField(max_length=60, help_text='Required')
-#
-#     class Meta:
-#         model = Account
-#         fields = ("email", "username", "employee_id", "password", "re_password")
-#
-#
-# class StudentRegistration(UserCreationForm):
-#     email = forms.EmailField(max_length=60, help_text='Required')
-#
-#     class Meta:
-#         model = Account
-#         fields = ('email', 'name', 'student_id', 'password', 're_password')
 
 class TeacherRegistration(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Add a valid email address.')
@@ -32,3 +18,33 @@ class StudentRegistration(UserCreationForm):
     class Meta:
         model = Account
         fields = ('email', 'username', 'student_id', 'password1', 'password2',)
+
+
+class StudentLoginForm(forms.Form):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ('student_id', 'password')
+
+        def clean(self):
+            if self.is_valid():
+                student_id = self.cleaned_data['student_id']
+                password = self.cleaned_data['password']
+                if not authenticate(student_id=student_id, password=password):
+                    raise forms.ValidationError("Invalid login")
+
+
+class TeacherLoginForm(forms.Form):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ('employee_id', 'password')
+
+        def clean(self):
+            if self.is_valid():
+                employee_id = self.cleaned_data['employee_id']
+                password = self.cleaned_data['password']
+                if not authenticate(employee_id=employee_id, password=password):
+                    raise forms.ValidationError("Invalid login")
