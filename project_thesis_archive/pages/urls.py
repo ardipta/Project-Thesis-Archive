5036cb099c10405f15b3178cd7679d59d0a70583
ardipto <ardipta82@gmail.com>
2020-08-23 21:31:49 +0600

    student dashboard mastering completed. modified some files.

--- project_thesis_archive/pages/urls.py
+++ project_thesis_archive/pages/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('student_dashboard', views.student, name='student_dashboard'),
+    path('student_dashboard/', views.student_choice, name='student_dashboard_choice'),
+    path('student_dashboard/project_upload/', views.project_upload, name='project_upload'),
+    path('student_dashboard/thesis_upload/', views.thesis_upload, name='thesis_upload'),
 ]
