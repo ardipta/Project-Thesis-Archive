05a7ac99646df57bda44a76eed4bcf74deffa3ca
ashikbhuyan <ashikbhuyan.swe.diu@gmail.com>
2020-08-28 19:19:18 +0600

    template mastering =>students app

--- project_thesis_archive/accounts/forms.py
+++ project_thesis_archive/accounts/forms.py
@@ -25,13 +25,13 @@ class StudentLoginForm(forms.Form):
 
     class Meta:
         model = Account
-        fields = ('student_id', 'password')
+        fields = ('email', 'password')
 
         def clean(self):
             if self.is_valid():
-                student_id = self.cleaned_data['student_id']
+                email = self.cleaned_data['email']
                 password = self.cleaned_data['password']
-                if not authenticate(student_id=student_id, password=password):
+                if not authenticate(email=email, password=password):
                     raise forms.ValidationError("Invalid login")
 
 
@@ -40,11 +40,11 @@ class TeacherLoginForm(forms.Form):
 
     class Meta:
         model = Account
-        fields = ('employee_id', 'password')
+        fields = ('email', 'password')
 
         def clean(self):
             if self.is_valid():
-                employee_id = self.cleaned_data['employee_id']
+                email = self.cleaned_data['email']
                 password = self.cleaned_data['password']
-                if not authenticate(employee_id=employee_id, password=password):
+                if not authenticate(email=email, password=password):
                     raise forms.ValidationError("Invalid login")
